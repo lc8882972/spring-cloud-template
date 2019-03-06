291775e0bc90b1b38b40ff533216e18474205f0f
zgy <410115424@qq.com>
2019-03-06 18:04:05 +0800

    删除旧版项目

--- sb/src/main/java/com/yan/sb/SbApplication.java
+++ sb/src/main/java/com/yan/sb/SbApplication.java
@@ -1,8 +1,10 @@
 package com.yan.sb;
 
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.alibaba.sentinel.annotation.SentinelRestTemplate;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +17,18 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class SbApplication {
-	@LoadBalanced
 	@Bean
+	@LoadBalanced
+	@SentinelRestTemplate(fallback = "fallbackHandler")
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
+	public String fallbackHandler(long s, BlockException ex) {
+		// Do some log here.
+		ex.printStackTrace();
+		return "Oops, error occurred at " + s;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SbApplication.class, args);
 	}
